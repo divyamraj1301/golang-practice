685b29a0b4640197aa1b5335c7c788c259dfdaf4
divyamraj1301 <divyamraj1301@gmail.com>
2024-02-22 16:59:35 +0530

    golang first commit

---
+++ 01hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+// "go mod init" - similar to npm init
+
+import "fmt" //this is auto imported by the go compiler when you import fmt in your code.
+
+func main() {
+	fmt.Println("Hello from User")
+}
+
+// to print, type "go run filename" on terminal
